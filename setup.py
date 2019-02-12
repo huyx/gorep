d6a738e24f6f907f9d5ecbf3a801757c91237a2c
ycyuxin <ycyuxin@gmail.com>
2019-02-12 15:34:31 +0800

    完善 setup.py，增加 long_description 等

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='gorep',
-    version='0.1',
+    version='0.1.1',
     description='Gor Grep Middleware.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
     author='ycyuxin',
     author_email='ycyuxin@qq.com',
+    url='https://github.com/huyx/gorep',
+
     scripts=['gorep.py'],
     entry_points='''
     [console_scripts]
